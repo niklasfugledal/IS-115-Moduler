e7258bed3cdd551a8a5b84ed6e47a11111bf7f40
niklasfugledal <79586171+niklasfugledal@users.noreply.github.com>
2022-10-20 16:11:10 +0200

    små endringer i modul 5

--- Modul5/Oppgave5.php
+++ Modul5/Oppgave5.php
@@ -3,16 +3,12 @@
 <?php
 //tekst som skal krypteres
 $string = "Niklas Fugledal";
-
 //Algoritme for kryptering
 $method = "AES-256-CBC";
-
 //nøkkelen for kryptering og dekryptering
 $key = "wasd";
-
 //Inneholder intialiseringsvektoren
 $iv = "1234567891011121";
-
 //bitwise disjunction
 $options = 0;
 
@@ -21,11 +17,9 @@ echo "Innputt: $string <br>x";
 
 function encryption($string, $key) : string
 {
-
     //Metode som krypterer dataen
     return openssl_encrypt($string, $GLOBALS["method"], $key, $GLOBALS["options"], $GLOBALS["iv"]);
 }
-
 
 $encryptedText = encryption($string, $key);
 //printer ut verdien
