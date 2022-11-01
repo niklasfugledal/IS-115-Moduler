c4d22273d7427e2a70b5868353b8dfef538ee92f
niklasfugledal <79586171+niklasfugledal@users.noreply.github.com>
2022-11-01 10:42:59 +0100

    changes

--- Modul6/Oppgave3.php
+++ Modul6/Oppgave3.php
@@ -1,23 +1,6 @@
 <h1>Oppgave 3</h1>
 
 <?php
-class User 
-{
-    //properties
-    public $firstName;
-    public $lastName;
-    protected $userName;
-    protected $regDate;
-    
-    public function __construct($firstName, $lastName, $userName, $regDate)
-    {
-      $this->firstName = $firstName;
-      $this->lastName = $lastName;
-      $this->userName = $userName;
-      $this->regDate = $regDate;
-    }
-    // Lage __constructor?????
-}
 
 ?>
 
